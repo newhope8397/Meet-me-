0f7087e7752972db7f7079a55e5b153bdfc47adb
newhope8397 <nhope8397@gmail.com>
2026-09-11 23:00:23 +0530

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCm2E61Ek-fY5xxAx48cIthDEsr05ajDds",
+  authDomain: "meet-me-1ee42.firebaseapp.com",
+  projectId: "meet-me-1ee42",
+  storageBucket: "meet-me-1ee42.firebasestorage.app",
+  messagingSenderId: "82016417313",
+  appId: "1:82016417313:web:097470da112ef3b1699560"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
