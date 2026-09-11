855c150308d6457ec9f58c6c3bd10e69f12faebf
newhope8397 <nhope8397@gmail.com>
2026-09-12 00:04:01 +0530

    Update firestore.js

--- js/firestore.js
+++ js/firestore.js
@@ -1,1 +1,11 @@
+import { app } from "./firebase.js";
 
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
+
+const db = getFirestore(app);
+
+
+export { db };
