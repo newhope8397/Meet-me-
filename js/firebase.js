5c20e60ba712ae3fcf728d99ecd0414935d0d398
newhope8397 <nhope8397@gmail.com>
2026-09-18 19:10:33 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,34 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
+import {
+    getAuth,
+    setPersistence,
+    browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
 const firebaseConfig = {
-apiKey: "AIzaSyCm2E61Ek-fY5xxAx48cIthDEsr05ajDds",
-  authDomain: "meet-me-1ee42.firebaseapp.com",
-  projectId: "meet-me-1ee42",
-  storageBucket: "meet-me-1ee42.firebasestorage.app",
-  messagingSenderId: "82016417313",
-  appId: "1:82016417313:web:097470da112ef3b1699560"
+    apiKey: "AIzaSyCm2E61Ek-fY5xxAx48cIthDEsr05ajDds",
+    authDomain: "meet-me-1ee42.firebaseapp.com",
+    projectId: "meet-me-1ee42",
+    storageBucket: "meet-me-1ee42.firebasestorage.app",
+    messagingSenderId: "82016417313",
+    appId: "1:82016417313:web:097470da112ef3b1699560"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export { app };
+const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence)
+    .catch(error => {
+        console.error(
+            "Could not enable login persistence:",
+            error
+        );
+    });
+
+export {
+    app,
+    auth
+};
